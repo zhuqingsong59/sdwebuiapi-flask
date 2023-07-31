c20873d0f2e5eda1e8420e76f08a73e26d16ae8e
zqs <zhuqingsong@weihaizixun.com>
2023-07-31 19:27:46 +0800

    调用翻译接口，翻译提示词

--- translate.py
+++ translate.py
@@ -6,10 +6,17 @@ from flask_restful import Resource
 class Translate(Resource):
     def post(self):
         request_json = request.json
-        textList = request_json['text'].split('，')
-        response = requests()
+        textList = request_json['textList']
         print(textList)
+        response = requests.post(
+            'http://127.0.0.1:7860/physton_prompt/translates', json={
+                'api': 'baidu_free',
+                'from_lang': 'zh_CN',
+                'to_lang': 'en_US',
+                'api_config': {},
+                'texts': textList
+            })
         return {
-            'data': 2333,
+            'data': response.json(),
             'code': 0
         }
