1cdb988ee90bb9a9ff132cd89a0654ddf0c0eb94
zqs <zhuqingsong@weihaizixun.com>
2023-11-28 16:11:56 +0800

    遮罩图合并代码优化

--- mergeMask.py
+++ mergeMask.py
@@ -8,37 +8,21 @@ from utils import base64_to_img, pil_to_base64
 class MergeMask(Resource):
     def post(self):
         request_json = request.json
+
         maskList = request_json['mask_list']
-        base_mask = Image.open(base64_to_img(maskList[0]))
-        new_mask = Image.new('RGBA', base_mask.size)
         output_path = './static/outputs/' + \
             time.strftime('%Y%m%d%H%M%S', time.localtime(
                 time.time())) + '_mask.png'
-        for mask_file in maskList:
-            mask = Image.open(base64_to_img(mask_file))
+
+        base_mask = Image.open(base64_to_img(maskList[0])).convert('RGBA')
+        for mask_file in maskList[1:]:
+            mask = Image.open(base64_to_img(mask_file)).convert('RGBA')
             assert mask.size == base_mask.size, "所有图片的大小需要相同"
-            for x in range(mask.width):
-                for y in range(mask.height):
-                    r_base, g_base, b_base, a_base = base_mask.getpixel((x, y))
-                    r_mask, g_mask, b_mask, a_mask = mask.getpixel((x, y))
 
-                    # 合并逻辑在此处定义，这里我们仍然使用逻辑或
-                    if a_base > 0 or a_mask > 0:
-                        if a_base > 0:
-                            new_mask.putpixel(
-                                (x, y), (r_base, g_base, b_base, a_base))
-                        else:
-                            new_mask.putpixel(
-                                (x, y), (r_mask, g_mask, b_mask, a_mask))
-                    else:
-                        new_mask.putpixel((x, y), (0, 0, 0, 0))
-            base_mask = new_mask
-
-        new_mask.save(output_path)
-
-        print(new_mask)
+            base_mask = Image.alpha_composite(base_mask, mask)
+        base_mask.save(output_path)
 
         return {
-            'data': 'data:image/png;base64,' + pil_to_base64(new_mask),
+            'data': 'data:image/png;base64,' + pil_to_base64(base_mask),
             'code': 0
         }
